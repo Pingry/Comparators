3d344de3822a3b51b583108f76528c792ec61865
BonnieBDev <jproudfoot2018@pingry.org>
2015-09-21 20:10:39 -0400

    Created comparator for CustomerRecord.

---
+++ CustomerPriorityComparator.java
@@ -0,0 +1,10 @@
+
+public class CustomerPriorityComparator <E extends CustomerRecord>{
+	public int comparePriority(E accountA, E accountB) {
+		/*Returns a number less than zero if the first account has a priority 
+		 * number less than the second, zero if they are the same, and a 
+		 * number greater than zero if the first priority number 
+		 * is greater than the second's */
+		return accountA.getPriority() - accountB.getPriority();
+	}
+}
