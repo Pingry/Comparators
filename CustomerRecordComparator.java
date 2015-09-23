2e470c084b0533c16d16175d2a32c5fd91aa1238
wfallon <wfallon2018@pingry.org>
2015-09-23 16:06:34 -0400

    CustomerRecordComparator 2
    Fixed simple bugs

--- CustomerRecordComparator.java
+++ CustomerRecordComparator.java
@@ -1,9 +1,11 @@
+import java.util.Comparator;
+
 public class CustomerRecordComparator extends CustomerRecord implements Comparator<CustomerRecord>
 {
       //Returns a positive number if customer 1's priority is higher than customer 2's priority
       //Returns 0  if customer 1's priority is the same as customer 2's priority
       //Returns a negative number if customer 1's priority is lower than customer 2's priority
-      public int compare (CustomerRecord c1, CustomerREcord c2)
+      public int compare (CustomerRecord c1, CustomeRecord c2)
       {
           return c1.getPriority() - c2.getPriority();
       }
