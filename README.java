f60bfd3bc688126b41db89f80ca14d1e8d01adc1
mtemares <mtemares2016@pingry.org>
2015-09-23 19:50:57 -0400

    Update and rename README.md to README.java
    i made a comparator that compares the amount the customer owes

--- README.java
+++ README.java
@@ -1,2 +1,10 @@
 # Comparators
 For this assignment, create a Comparator that does a useful comparison on the CustomerRecord class provided. Optionally, you can finish the Sorter.java class and actually use your comparator to sort a large list. Use the generateRandomRecord method in CustomerRecord to help.
+
+public class AmountOwedComparator implements Comparator<Customer Record>
+{
+  public int compare(Customer Record cr1, Customer Record cr2)
+  {
+    return cr1.getAmountOwed()-cr2.getAmountOwed();
+  }
+}
