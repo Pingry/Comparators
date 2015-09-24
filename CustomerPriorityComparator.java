ea8f5f5591ff5fc868cf27c12119037b2a99060b
jproudfoot <jproudfoot2018@pingry.org>
2015-09-24 10:48:27 -0400

    Update CustomerPriorityComparator.java

--- CustomerPriorityComparator.java
+++ CustomerPriorityComparator.java
@@ -1,5 +1,5 @@
 
-public class CustomerPriorityComparator <E extends CustomerRecord> implements Comparator{
+public class CustomerPriorityComparator <E extends CustomerRecord> implements Comparator<CustomerRecord>{
 	public int comparePriority(E accountA, E accountB) {
 		/*Returns a number less than zero if the first account has a priority 
 		 * number less than the second, zero if they are the same, and a 
